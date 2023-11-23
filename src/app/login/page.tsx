71cfdaed53682a32a95f2b8e22c2ac27ed580e94
Anjali Singh <105203288+Anjali0048@users.noreply.github.com>
2023-11-23 19:42:01 +0530

    login and signup basic ui setup

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,48 @@
 "use client"
+import Link from "next/link"
+import React from "react"
+import { useRouter } from "next/navigation"
+import { axios } from "axios"
 
 export default function LoginPage() {
+
+    const [user, setUser] = React.useState({
+        email: "",
+        password: "",
+    })
+
+    const onLogin = async () => {
+
+    }
+
     return (
-        <div>
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <h1>Login</h1>
+            <hr />
+            <label htmlFor="email">email</label>
+            <input 
+                type="text"
+                id="email" 
+                value={user.email}
+                onChange={(e) => setUser({...user, email:e.target.value})}
+                placeholder="email"
+            />
+            <hr />
+            <label htmlFor="password">password</label>
+            <input 
+                type="password"
+                id="password" 
+                value={user.password}
+                onChange={(e) => setUser({...user, password:e.target.value})}
+                placeholder="password"
+            />
+            <button 
+                onClick={onLogin}
+                className="border-gray-300 border p-2 rounded-lg m-2"
+            >
+                Login here
+            </button>
+            <Link href="/signup">visit Signup</Link>
         </div>
     )
 }
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -6,5 +6,53 @@ import { axios } from "axios"
 
 export default function SignupPage() {
 
-    
+    const [user, setUser] = React.useState({
+        email: "",
+        password: "",
+        username: "",
+    })
+
+    const onSignup = async () => {
+
+    }
+
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
+            <h1>Signup</h1>
+            <hr />
+            <label htmlFor="username">username</label>
+            <input 
+                type="text"
+                id="username" 
+                value={user.username}
+                onChange={(e) => setUser({...user, username:e.target.value})}
+                placeholder="username"
+            />
+            <hr />
+            <label htmlFor="email">email</label>
+            <input 
+                type="text"
+                id="email" 
+                value={user.email}
+                onChange={(e) => setUser({...user, email:e.target.value})}
+                placeholder="email"
+            />
+            <hr />
+            <label htmlFor="password">password</label>
+            <input 
+                type="password"
+                id="password" 
+                value={user.password}
+                onChange={(e) => setUser({...user, password:e.target.value})}
+                placeholder="password"
+            />
+            <button 
+                onClick={onSignup}
+                className="border-gray-300 border p-2 rounded-lg m-2"
+            >
+                Signup here
+            </button>
+            <Link href="/login">visit Login</Link>
+        </div>
+    )
 }
